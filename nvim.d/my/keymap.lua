50203b2efcde7f27efb3e1e7c19edaee8acf410f
don <man.hong.ch@gmail.com>
2022-04-17 14:49:00 +0800

    update config for mac

--- nvim.d/my/keymap.lua
+++ nvim.d/my/keymap.lua
@@ -1,6 +1,6 @@
 local opts = { noremap = true, silent = true }
 -- local term_opts = { silent = true }
-local map = vim.keymap.set
+local map = vim.api.nvim_set_keymap
 local cancel = function(tbl, keys)
   for i, k in pairs(keys) do
     map(tbl, k, "<nop>", opts)
@@ -20,34 +20,35 @@ map("n", "<c-j>", "<<", opts)
 map("n", "<c-k>", ">>", opts)
 map("n", "j", "gj", opts)
 map("n", "k", "gk", opts)
-map("n", "hu", ":")
-map("n", "hj", "/")
-map("n", "HJ", "?")
+map("n", "hu", ":", {noremap = true})
+map("n", "hj", "/", {noremap = true})
+map("n", "HJ", "?", {noremap = true})
 map("n", "x", '"_x', opts)
 map("n", "<m-a>", "ggVG", opts)
+
+-- map("n", "<m-h>", ":tabp<cr>", opts)
+-- map("n", "<m-l>", ":tabn<cr>", opts)
+-- map("n", "qf", ":q<cr>", opts)
 map("n", "<m-h>", ":BufferLineCyclePrev<cr>", opts)
 map("n", "<m-l>", ":BufferLineCycleNext<cr>", opts)
-map("n", "<leader><m-h>", ":BufferLineMovePrev<cr>", opts)
-map("n", "<leader><m-l>", ":BufferLineMoveNext<cr>", opts)
+-- nnoremap <silent><mymap> :BufferLineMoveNext<CR>
+-- nnoremap <silent><mymap> :BufferLineMovePrev<CR>
 map("n", "<m-b>", "<cmd>Telescope buffers theme=dropdown<cr>", opts)
 map("n", "qf", "<cmd>bwipeout<cr>", opts)
 map("n", "<leader>cl", ":let @+ = expand('%:p')<cr>", opts)     -- copy current file path to clipboard
-map("n", "ss", require("my.send").send_current_line, opts)
-map("n", "sb", require("my.send").send_last_line, opts)
+map("n", "ss", 'yy:call system("paste_to_next_tmux_pane")<cr>', opts)
+map("n", "sb", ':call system("paste_to_next_tmux_pane last-cmd")<cr>', opts)
 map("n", "gV", '`[v`]', opts)
 map("n", "<leader>ps", ':PackerSync<cr>', opts)
 map("n", "hl", "<cmd>Telescope find_files<cr>", opts)
 map("n", "<leader>ca", "<cmd>Telescope live_grep<cr>", opts)
 map("n", "<c-f>", "<cmd>Telescope grep_string<cr>", opts)
+map("n", "sf", "gcc", {})
 map("n", "<c-t>", "<cmd>NvimTreeToggle<cr>", opts)
 map("n", "<leader>t", "<cmd>TSPlaygroundToggle<cr>", opts)
-map("n", "<leader>hg", '<cmd>echo synIDattr(synID(line("."),col("."),1),"name")<cr>', opts)
-map("n", "<leader>tl", '<cmd>lua toggle_lsp()<cr>', opts)
-map("n", "c*", '*Ncgn', opts)        -- c* to edit word under cursor, repeatable by the dot key
-map("n", "d*", '*Ndgn', opts)
-map("n", "c.", '<cmd>let @/=@"<cr>/<cr>cgn<c-r>.<esc>', opts)       -- c. to make the last edit repeatable by dot
-map("n", "d.", '<cmd>let @/=@"<cr>/<cr>dgn<c-r>.<esc>', opts)
-cancel("n", {">>", "<<", ":", "/", "?", "Q", "qq", "<bs>", "<del>", "<cr>", "<up>", "<down>", "<left>", "<right>"})
+-- cancel("n", {">>", "<<", ":", "/", "?", "Q", "qq", "<bs>", "<del>", "<cr>", "<up>", "<down>", "<left>", "<right>"})
+-- map("n", "<C-h>", "<C-w>h", opts)    -- switch window
+-- map("n", "<C-l>", "<C-w>l", opts)
 
 -- Insert
 map("i", "jk", "<esc>", opts)
@@ -57,17 +58,14 @@ map("i", "<c-n>", "<Cmd>lua require('cmp').complete()<CR>", opts)
 cancel("i", {"<esc>", "<del>", "<cr>", "<up>", "<down>", "<left>", "<right>"})
 
 -- Visual
-map("x", "<c-j>", "<gv", opts)
-map("x", "<c-k>", ">gv", opts)
-map("x", "hu", ":")
-map("x", "p", '"_dP', opts)
-map("x", "s", '<cmd>lua require("my.send").send_highlighted_lines()<cr>', opts)
-map("x", "sf", "<Plug>(comment_toggle_linewise_visual)", opts)
-map("x", "<c-f>", function()
-  vim.cmd([[normal "ay]])
-  vim.cmd("Telescope live_grep default_text=" .. vim.fn.getreg("a"))
-end)
-cancel("x", {">", "<", "<esc>", ":"})
+map("v", "<c-j>", "<gv", opts)
+map("v", "<c-k>", ">gv", opts)
+map("v", "hu", ":", {noremap = true})
+map("v", "p", '"_dP', opts)
+map("v", "ss", 'y:call system("paste_to_next_tmux_pane")<cr>', opts)
+map("v", "sf", "gc", {})
+map("v", "<c-f>", "y<esc><cmd>Telescope live_grep default_text=<c-r>0<cr>", opts)
+cancel("v", {">", "<", "<esc>", ":"})
 
 -- Command
 map("c", "jk", "<c-c>", opts)
